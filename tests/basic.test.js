f3f6e7bf1cb4e44285b3f4d60e6cb7143b55643e
Juan Aguirre <j1927aguirre@gmail.com>
2021-12-07 18:48:50 -0500

    Add expect method

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -3,7 +3,13 @@ import { Selector } from 'testcafe';
 fixture`Getting started with TestCafe`.page(
 	`https://devexpress.github.io/testcafe/example/`
 );
-test('My first testcafe test', async (t) => {
-	await t.typeText('#developer-name', 'John');
+test('My first testcafe test', async t => {
+	// Arrange
+	const name = 'Juan';
+
+	// Act
+	await t.typeText('#developer-name', name);
 	await t.click('#submit-button');
+	// Assert
+	await t.expect(Selector('#article-header').innerText).contains(name);
 });
