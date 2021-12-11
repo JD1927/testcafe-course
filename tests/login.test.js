4f0ec5738d6ad3b6b2103e0e75d398cf07ca616c
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-11 11:40:58 -0500

    E2E 1: Login to the application

---
+++ tests/login.test.js
@@ -0,0 +1,25 @@
+import { Selector } from 'testcafe';
+
+// prettier-ignore
+fixture`Login Test`
+  .page(`http://zero.webappsecurity.com/index.html`);
+
+test('User cannot login with invalid credentials', async t => {
+	const signInButton = Selector('#signin_button');
+	await t.click(signInButton);
+
+	const loginForm = Selector('#login_form');
+	await t.expect(loginForm.exists).ok();
+
+	const usernameInput = Selector('#user_login');
+	const passwordInput = Selector('#user_password');
+
+	await t.typeText(usernameInput, 'invalid username', { paste: true });
+	await t.typeText(passwordInput, 'invalid password', { paste: true });
+
+	const submitButton = Selector('input[name="submit"]');
+	await t.click(submitButton);
+
+	const errorMessage = Selector('.alert.alert-error').innerText;
+	await t.expect(errorMessage).contains('Login and/or password are wrong.');
+});
