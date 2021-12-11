6f570b0ecba53aa654a68b0b118a73ccbe9ef5ce
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-11 16:52:45 -0500

    E2E 1: Login to the application Part 2

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 // prettier-ignore
 fixture`Login Test`
-  .page(`http://zero.webappsecurity.com/index.html`);
+  .page`http://zero.webappsecurity.com/index.html`;
 
 test('User cannot login with invalid credentials', async t => {
 	const signInButton = Selector('#signin_button');
@@ -22,4 +22,34 @@ test('User cannot login with invalid credentials', async t => {
 
 	const errorMessage = Selector('.alert.alert-error').innerText;
 	await t.expect(errorMessage).contains('Login and/or password are wrong.');
+});
+
+test('User can login into application', async t => {
+	const signInButton = Selector('#signin_button');
+	await t.click(signInButton);
+
+	const loginForm = Selector('#login_form');
+	await t.expect(loginForm.exists).ok();
+
+	const usernameInput = Selector('#user_login');
+	const passwordInput = Selector('#user_password');
+
+	await t.typeText(usernameInput, 'username', { paste: true });
+	await t.typeText(passwordInput, 'password', { paste: true });
+
+	const submitButton = Selector('.btn.btn-primary');
+	await t.click(submitButton);
+
+	const accountSummaryTab = Selector('#account_summary_tab');
+	await t.expect(accountSummaryTab.exists).ok();
+	await t.expect(loginForm.exists).notOk();
+
+	const userDropDown = Selector('.icon-user');
+	await t.click(userDropDown);
+
+	const logoutButton = Selector('#logout_link');
+	await t.click(logoutButton);
+
+	await t.expect(logoutButton.exists).notOk();
+	await t.expect(signInButton.exists).ok();
 });
